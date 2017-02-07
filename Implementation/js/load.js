0556063171ac584e04809ae59b0c8bb765be1e7d
Joshua Petherick <peth@live.co.uk>
2017-02-07 20:06:08 +0000

    Editted index.html

--- Implementation/js/load.js
+++ Implementation/js/load.js
@@ -2,13 +2,13 @@
 
 var loadState = {
   preload: function() {
-      game.load.tilemap('map1', 'assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
-      game.load.image('tiles1', 'assets/tiles1.png');
-      game.load.tilemap('map2', 'assets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON);
-      game.load.image('tiles2', 'assets/tiles2.png');
-      game.load.spritesheet('level1_btn', 'assets/Level1_button.png', 200, 133, 2);
-      game.load.spritesheet('level2_btn', 'assets/Level2_button.png', 200, 133, 2);
-      game.load.image('player', 'assets/playerTest.png');
+      game.load.tilemap('map1', 'Implementation/assets/levels/level1.json', null, Phaser.Tilemap.TILED_JSON);
+      game.load.image('tiles1', 'Implementation/assets/tiles1.png');
+      game.load.tilemap('map2', 'Implementation/assets/levels/level2.json', null, Phaser.Tilemap.TILED_JSON);
+      game.load.image('tiles2', 'Implementation/assets/tiles2.png');
+      game.load.spritesheet('level1_btn', 'Implementation/assets/Level1_button.png', 200, 133, 2);
+      game.load.spritesheet('level2_btn', 'Implementation/assets/Level2_button.png', 200, 133, 2);
+      game.load.image('player', 'Implementation/assets/playerTest.png');
   },
 
   create: function() {
