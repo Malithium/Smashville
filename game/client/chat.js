f022f0981a012075b5c91b7af0f789e96561988d
Kyle Tuckey <malithium@gmail.com>
2017-02-15 01:41:54 +0000

    added in the sessions display, and a dummy host button for later

--- game/client/chat.js
+++ game/client/chat.js
@@ -5,6 +5,7 @@
 var messages = [];
 var sessions = [];
 var numOfMessages = 0;
+var numOfSessions = 0;
 
 var chatState = {
     preload: function(){
@@ -14,6 +15,7 @@ var chatState = {
 
     create: function () {
         numOfMessages = messages.length;
+
         nameLabel = game.add.text(10, 10, 'Chat', {font: '30px Arial', fill: '#ffffff'});
         d = document.getElementById('chat-overlay');
         d.style.left = GAMEWIDTH / 8 + 'px';
@@ -41,6 +43,39 @@ var chatState = {
             this.style.backgroundColor = 'transparent';
             this.style.color = 'white';
         };
+
+        s = document.getElementById('session-overlay');
+        s.style.left = GAMEWIDTH - (300 + (GAMEWIDTH / 8)) + 'px';
+        s.style.top = GAMEHEIGHT/ 8 + 'px';
+        s.style.display = 'block';
+        for(var i = 0; i < sessions.length; i++)
+        {
+            sessions[i]
+        }
+        sessionCol = document.getElementsByClassName('session');
+
+        for(var i = 0; i < sessionCol.length; i++)
+        {
+            console.log(sessionCol[i]);
+            sessionCol[i].onmouseover = function (){
+                this.style.backgroundColor = 'white';
+                this.style.color = 'black';
+            };
+
+            sessionCol[i].onmouseout = function (){
+                this.style.backgroundColor = 'transparent';
+                this.style.color = 'white';
+            };
+
+            sessionCol[i].onclick = function (){
+                //var innerName = sessionCol[i].getElementsByClassName('session-name');
+                var innerName = this.getElementsByClassName('session-name')[0].innerText;
+                console.log(innerName);
+                //socket.emit('join session', {name: innerName.toString()});
+            };
+        }
+
+
     },
 
     update: function () {
@@ -52,5 +87,14 @@ var chatState = {
                  m.innerHTML += msg;
              });
          }
-        }
+    },
+
+    hostSession: function(){
+        socket.emit('new session', {id: this.id, name: playerName});
+        levelNum = 1;
+        playerNum = 1;
+        d = document.getElementById('chat-overlay').style.display = 'none';
+        s = document.getElementById('session-overlay').style.display = 'none';
+        game.state.start('play');
     }
+}
